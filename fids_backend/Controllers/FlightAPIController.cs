dd1e5e6f48c3223a3f70f04d2b71a64e6de3b7ad
Divyash Chhetri <divyashchhetri@gmail.com>
2022-10-15 01:59:10 +0600

    API Complete

---
+++ fids_backend/Controllers/FlightAPIController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using fids_backend.Models;
+
+namespace fids_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlightAPIController : ControllerBase
+    {
+        private readonly fidsContext _context;
+
+        public FlightAPIController(fidsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FlightAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FlightDetail>>> GetFlightDetails()
+        {
+          if (_context.FlightDetails == null)
+          {
+              return NotFound();
+          }
+            return await _context.FlightDetails.ToListAsync();
+        }
+
+        // GET: api/FlightAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<FlightDetail>> GetFlightDetail(int id)
+        {
+          if (_context.FlightDetails == null)
+          {
+              return NotFound();
+          }
+            var flightDetail = await _context.FlightDetails.FindAsync(id);
+
+            if (flightDetail == null)
+            {
+                return NotFound();
+            }
+
+            return flightDetail;
+        }
+
+        // PUT: api/FlightAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFlightDetail(int id, FlightDetail flightDetail)
+        {
+            if (id != flightDetail.FlightId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(flightDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FlightDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/FlightAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<FlightDetail>> PostFlightDetail(FlightDetail flightDetail)
+        {
+          if (_context.FlightDetails == null)
+          {
+              return Problem("Entity set 'fidsContext.FlightDetails'  is null.");
+          }
+            _context.FlightDetails.Add(flightDetail);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFlightDetail", new { id = flightDetail.FlightId }, flightDetail);
+        }
+
+        // DELETE: api/FlightAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFlightDetail(int id)
+        {
+            if (_context.FlightDetails == null)
+            {
+                return NotFound();
+            }
+            var flightDetail = await _context.FlightDetails.FindAsync(id);
+            if (flightDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.FlightDetails.Remove(flightDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FlightDetailExists(int id)
+        {
+            return (_context.FlightDetails?.Any(e => e.FlightId == id)).GetValueOrDefault();
+        }
+    }
+}

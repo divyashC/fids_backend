dae69af1f1820fd64f108ed9d264195dfbbc8cd2
Divyash Chhetri <divyashchhetri@gmail.com>
2022-10-19 13:11:06 +0600

    API Updated

--- fids_backend/Controllers/FlightAPIController.cs
+++ fids_backend/Controllers/FlightAPIController.cs
@@ -56,46 +56,206 @@ namespace fids_backend.Controllers
             
             return dataSetB;
         }
+        
+        // GET: api/FlightAPI/search/past/{search}
+        [HttpGet("search/past/{search}")]
+        public async Task<ActionResult<IEnumerable<FlightDetail>>> GetPastFlight(string search)
+        {
+            var flight = await _context.FlightDetails.Where(f => f.Origin.Contains(search) || f.Destination.Contains(search)).ToListAsync();
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            
+            var dataSet = new List<FlightDetail>(); // old flights
+
+            foreach (var item in flight)
+            {
+                if (item.FlightDate < DateTime.Now && item.FlightDate.ToString("dd/MM/yyyy") != DateTime.Now.ToString("dd/MM/yyyy"))
+                {
+                    dataSet.Add(item);
+                }
+            }
+            
+            return dataSet;
+        }
+        
+        // GET: api/FlightAPI/search/upcoming/{search}
+        [HttpGet("search/upcoming/{search}")]
+        public async Task<ActionResult<IEnumerable<FlightDetail>>> GetUpcomingFlight(string search)
+        {
+            var flight = await _context.FlightDetails.Where(f => f.Origin.Contains(search) || f.Destination.Contains(search)).ToListAsync();
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            
+            var dataSet = new List<FlightDetail>(); // old flights
+
+            foreach (var item in flight)
+            {
+                if (item.FlightDate < DateTime.Now && item.FlightDate.ToString("dd/MM/yyyy") != DateTime.Now.ToString("dd/MM/yyyy"))
+                {
+                    dataSet.Add(item);
+                }
+            }
+            
+            return dataSet;
+        }
+        
+        // GET: api/FlightAPI/search/today/{search}
+        [HttpGet("search/today/{search}")]
+        public async Task<ActionResult<IEnumerable<FlightDetail>>> GetTodaysFlight(string search)
+        {
+            var flight = await _context.FlightDetails.Where(f => f.Origin.Contains(search) || f.Destination.Contains(search)).ToListAsync();
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            
+            var dataSet = new List<FlightDetail>(); // old flights
+
+            foreach (var item in flight)
+            {
+                if (item.FlightDate > DateTime.Now && item.FlightDate != DateTime.Now)
+                {
+                    dataSet.Add(item);
+                }
+            }
+            
+            return dataSet;
+        }
 
         // GET: api/FlightAPI
         [HttpGet]
         public async Task<ActionResult<IEnumerable<FlightDetail>>> GetFlightDetails()
         {
-          if (_context.FlightDetails == null)
-          {
-              return NotFound();
-          }
-          var flightData = await _context.FlightDetails.ToListAsync();
+            if (_context.FlightDetails == null)
+            {
+                return NotFound();
+            }
+            var flightData = await _context.FlightDetails.ToListAsync();
           
-          var dataSetA = new List<FlightDetail>();
-          var dataSetB = new List<FlightDetail>();
+            var dataSetA = new List<FlightDetail>();
+            var dataSetB = new List<FlightDetail>();
 
-          foreach (var flight in flightData)
-          {
-              if (flight.FlightDate >= DateTime.Now)
-              {
-                  dataSetA.Add(flight);
-              }
-          }
+            foreach (var flight in flightData)
+            {
+                if (flight.FlightDate >= DateTime.Now)
+                {
+                    dataSetA.Add(flight);
+                }
+            }
           
-          foreach (var flight in flightData)
-          {
-              if (flight.FlightDate.Date == DateTime.Now.Date)
-              {
-                  if (flight.DepartureTime.Add(new TimeSpan(0, 2, 0)) < DateTime.Now.TimeOfDay) 
-                  { 
-                      continue;
-                  } 
-                  dataSetB.Add(flight);
-              }
-          }
+            foreach (var flight in flightData)
+            {
+                if (flight.FlightDate.Date == DateTime.Now.Date)
+                {
+                    if (flight.DepartureTime.Add(new TimeSpan(0, 2, 0)) < DateTime.Now.TimeOfDay) 
+                    { 
+                        continue;
+                    } 
+                    dataSetB.Add(flight);
+                }
+            }
           
-          dataSetB.AddRange(dataSetA);
-          dataSetB = dataSetB.OrderBy(f => f.FlightDate).ToList();
+            dataSetB.AddRange(dataSetA);
+            dataSetB = dataSetB.OrderBy(f => f.FlightDate).ToList();
           
-          return dataSetB.Take(30).ToList();
+            return dataSetB.Take(30).ToList();
 
         }
+        
+        // GET: api/FlightAPI/upcoming
+        [HttpGet("upcoming")]
+        public async Task<ActionResult<IEnumerable<FlightDetail>>> GetUpcomingFlightDetails()
+        {
+            if (_context.FlightDetails == null)
+            {
+                return NotFound();
+            }
+            var flight = await _context.FlightDetails.ToListAsync();
+          
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            
+            var dataSet = new List<FlightDetail>(); // old flights
+
+            foreach (var item in flight)
+            {
+                if (item.FlightDate > DateTime.Now && item.FlightDate != DateTime.Now)
+                {
+                    dataSet.Add(item);
+                }
+            }
+            
+            return dataSet;
+
+        }
+        
+        // GET: api/FlightAPI/past
+        [HttpGet("past")]
+        public async Task<ActionResult<IEnumerable<FlightDetail>>> GetPastFlightDetails()
+        {
+            if (_context.FlightDetails == null)
+            {
+                return NotFound();
+            }
+            var flight = await _context.FlightDetails.ToListAsync();
+          
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            
+            var dataSet = new List<FlightDetail>(); // old flights
+
+            foreach (var item in flight)
+            {
+                if (item.FlightDate < DateTime.Now && item.FlightDate.ToString("dd/MM/yyyy") != DateTime.Now.ToString("dd/MM/yyyy"))
+                {
+                    dataSet.Add(item);
+                }
+            }
+            
+            return dataSet;
+
+        }
+        
+        // GET: api/FlightAPI/today
+        [HttpGet("today")]
+        public async Task<ActionResult<IEnumerable<FlightDetail>>> GetTodaysFlightDetails()
+        {
+            if (_context.FlightDetails == null)
+            {
+                return NotFound();
+            }
+            var flight = await _context.FlightDetails.ToListAsync();
+          
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            
+            var dataSet = new List<FlightDetail>(); // old flights
+
+            foreach (var item in flight)
+            {
+                if (item.FlightDate > DateTime.Now && item.FlightDate != DateTime.Now)
+                {
+                    dataSet.Add(item);
+                }
+            }
+            
+            return dataSet;
+
+        }
+        
 
         // GET: api/FlightAPI/5
         [HttpGet("{id}")]
